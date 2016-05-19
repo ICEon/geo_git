fed28f6a5fd1a594b4ca1aa5f1baf329dc89b58b
ICEon <matrix_eon@yahoo.com>
2016-05-19 13:31:07 -0500

    tres

--- js/acciones.js
+++ js/acciones.js
@@ -3,21 +3,24 @@ $(document).ready(function(e) {
  document.addEventListener("deviceready", onDeviceReady, false);
  function onDeviceReady() {
 	 
-	 $('#localizar').on('tap', function (){
-		navigator.geolocation.getCurrentPosition( onSuccess, $('#localizacion').html('Error en la localizacion') );
-	 });
+	function onSuccess(position) {
+    
+    $('#localizacion').html ( 'Latitude: '  + position.coords.latitude      + '<br />' +
+                        'Longitude: ' + position.coords.longitude     + '<br />' +
+                        '<hr />'      + element.innerHTML);
+}
 
- }
- 
- var onSuccess = function (position){ 
-		 $('#localizacion').html('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+// Options: throw an error if no update is received every 30 seconds.
+//
+var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+
 		}
  
 });
